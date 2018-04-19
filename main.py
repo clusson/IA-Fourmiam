ee4d8f4310f1489d6479557a193e6cbba1e11d67
Clément Lusson <clementlusson@gmail.com>
2018-04-19 19:26:10 +0200

    updatesé

--- main.py
+++ main.py
@@ -17,11 +17,14 @@ CONST_BP_MIN        = 'BP_MIN'
 CONST_BP_MAX        = 'BP_MAX'
 CONST_STATUT        = 'STATUT'
 
+
 def nodes(node_x, node_y):
     dist = m.sqrt(pow(node_x[1] - node_y[1], 2) + pow(node_x[1] + node_y[1], 2))
     return dist
 
 # Création du chemin noeud par noeud
+
+
 def find_path(graph, start, end, path=[]):
     road = path + [start]
     if start == end:
@@ -49,25 +52,26 @@ with open('VOIES_NM.csv', encoding='utf-8') as csvFile:
     i = 0
     for row in reader:
         i += 1
+        # On parcourt les 150 premières lignes
         if i < 150:
             ADRESS = row[CONST_COMMUNE] + '' + row[CONST_LIBELLE]
             TENANT = row[CONST_TENANT]
             ABOUTISSANT = row[CONST_ABOUTISSANT]
             STATUT = row[CONST_STATUT]
             if TENANT != "" and ABOUTISSANT != "":
-                    if (row[CONST_BI_MIN] == ""):
+                    if row[CONST_BI_MIN] == "":
                         row[CONST_BI_MIN] = 1
                     else:
                         row[CONST_BI_MIN] = int(row[CONST_BI_MIN])
-                    if (row[CONST_BP_MIN] == ""):
+                    if row[CONST_BP_MIN] == "":
                         row[CONST_BP_MIN] = 1
                     else:
                         row[CONST_BP_MIN] = int(row[CONST_BP_MIN])
-                    if (row[CONST_BI_MAX] == ""):
+                    if row[CONST_BI_MAX] == "":
                         row[CONST_BI_MAX] = 1
                     else:
                         row[CONST_BI_MAX] = int(row[CONST_BI_MAX])
-                    if (row[CONST_BP_MAX] == ""):
+                    if row[CONST_BP_MAX] == "":
                         row[CONST_BP_MAX] = 1
                     else:
                         row[CONST_BP_MAX] = int(row[CONST_BP_MAX])
@@ -95,19 +99,3 @@ network.draw_networkx_edge_labels(G, pos)
 network.draw_networkx_nodes(G, pos)
 pyp.show()
 
-nodes = []
-choice = r.randint(1, 10)
-
-for i in range(choice):
-    x = r.uniform(0, 10)
-    y = r.uniform(0, 10)
-    nodes.append([x, y])
-
-ensemble = p.World(nodes, function)
-solver = p.Solver()
-solutions = solver.solutions(ensemble)
-
-for solution in solutions:
-    print(solution.distance)
-
-print(nodes)
